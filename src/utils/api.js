a4359198bce3ffa6184b642533f42340c042ef24
Manoj <manojddev20@gmail.com>
2026-03-27 05:39:25 +0530

    v1

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://maya143.app.n8n.cloud/webhook';
+const BASE_URL = 'https://maya143.app.n8n.cloud/webhook-test';
 
 const api = async (endpoint, options = {}) => {
   const res = await fetch(`${BASE_URL}${endpoint}`, {
